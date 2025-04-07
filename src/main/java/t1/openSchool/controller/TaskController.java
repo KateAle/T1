54831542c45fa12196811123f1c75abc6922be38
Ekaterina Shulenina <shulenina.em@gmail.com>
2025-04-07 22:24:56 +0300

    AOP- задача 1 - code review comments

--- src/main/java/t1/openSchool/controller/TaskController.java
+++ src/main/java/t1/openSchool/controller/TaskController.java
@@ -2,7 +2,6 @@ package t1.openSchool.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,12 +9,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import t1.openSchool.dto.TaskDto;
 import t1.openSchool.service.TaskService;
 
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/tasks")
@@ -25,30 +24,31 @@ public class TaskController {
     private final TaskService taskService;
 
     @PostMapping
-    public ResponseEntity<TaskDto> createTask(@RequestBody TaskDto taskDto) {
-        TaskDto createdTask = taskService.createTask(taskDto);
-        return new ResponseEntity<>(createdTask, HttpStatus.CREATED);
+    @ResponseStatus(HttpStatus.CREATED)
+    public TaskDto createTask(@RequestBody TaskDto taskDto) {
+        return taskService.createTask(taskDto);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<TaskDto> getTask(@PathVariable Long id) {
-        TaskDto taskDto = taskService.getTask(id);
-        return taskDto != null ? ResponseEntity.ok(taskDto) : ResponseEntity.notFound().build();
+    @ResponseStatus(HttpStatus.OK)
+    public TaskDto getTask(@PathVariable Long id) {
+        return taskService.getTask(id);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<TaskDto> updateTask(@PathVariable Long id, @RequestBody TaskDto taskDto) {
-        TaskDto updatedTask = taskService.updateTask(id, taskDto);
-        return updatedTask != null ? ResponseEntity.ok(updatedTask) : ResponseEntity.notFound().build();
+    @ResponseStatus(HttpStatus.OK)
+    public TaskDto updateTask(@PathVariable Long id, @RequestBody TaskDto taskDto) {
+        return taskService.updateTask(id, taskDto);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTask(@PathVariable Long id) {
         taskService.deleteTask(id);
-        return ResponseEntity.noContent().build();
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<TaskDto> getAllTasks() {
         return taskService.getAllTasks();
     }
